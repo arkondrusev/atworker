d0834591e6564aa5197283f1b6ffb59470abfbd2
Artem <artem.kondrusev@gmail.com>
2022-04-18 16:00:15 +0300

    db connections

--- dev/src/main/java/com/sadkoala/arbitrator/atworker/ATWorkerApp.java
+++ dev/src/main/java/com/sadkoala/arbitrator/atworker/ATWorkerApp.java
@@ -2,6 +2,11 @@ package com.sadkoala.arbitrator.atworker;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.sqlite.SQLiteConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ATWorkerApp {
 
@@ -12,13 +17,42 @@ public class ATWorkerApp {
         log.info("At-worker started");
 
         // установление подключения к worker-db
+        String workerDbPath = "persistent_files\\at-worker.db";
+        Connection workerConnection = connectDB(workerDbPath, false);
+        if (workerConnection == null) {
+            log.error("Could not connect to worker DB at {" + workerDbPath + "}" );
+            log.fatal("At-worker app failed");
+        }
+        log.info("Worker DB connected {" + workerDbPath + "}");
         // установление ro-подключения к monitor-db
+        String monitorDbPath = "persistent_files\\at-monitor.db";
+        Connection monitorConnection = connectDB(monitorDbPath, true);
+        if (monitorConnection == null) {
+            log.error("Could not connect to monitor DB at {" + monitorDbPath + "}" );
+            log.fatal("At-worker app failed");
+        }
+        log.info("Monitor DB connected {" + monitorDbPath + "}");
         // установление подключения к binance
 
         // с момента подключения к worker-db логи писать туда
 
         log.info("At-worker finished");
 
+    }
+
+    private static Connection connectDB(String dbPath, boolean readonly) {
+        Connection connection = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:" + dbPath;
+            // create a connection to the database
+            SQLiteConfig config = new SQLiteConfig();
+            config.setReadOnly(readonly);
+            connection = DriverManager.getConnection(url, config.toProperties());
+        } catch (SQLException e) {
+            log.error(e);
+        }
+        return connection;
     }
 
 }

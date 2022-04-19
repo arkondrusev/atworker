67cce1fb39bbda194867dccdf0a45d0d7eccf8f5
Artem <artem.kondrusev@gmail.com>
2022-04-20 01:28:21 +0300

    - подключение к базам и отключение - подключение к вебсокету и отключение

--- dev/src/main/java/com/sadkoala/arbitrator/atworker/ATWorkerApp.java
+++ dev/src/main/java/com/sadkoala/arbitrator/atworker/ATWorkerApp.java
@@ -4,37 +4,61 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sqlite.SQLiteConfig;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class ATWorkerApp {
 
     private static final Logger log = LogManager.getLogger(ATWorkerApp.class);
 
+    public static Connection monitorDbConnection = null;
+    public static Connection workerDbConnection = null;
+    public static WebSocket webSocket = null;
+
     public static void main(String[] args) {
 
         log.info("At-worker started");
 
-        // установление подключения к worker-db
-        String workerDbPath = "persistent_files\\at-worker.db";
-        Connection workerConnection = connectDB(workerDbPath, false);
-        if (workerConnection == null) {
-            log.error("Could not connect to worker DB at {" + workerDbPath + "}" );
-            log.fatal("At-worker app failed");
+        monitorDbConnection = connectMonitorDB();
+        if (monitorDbConnection == null) {
+            return;
         }
-        log.info("Worker DB connected {" + workerDbPath + "}");
-        // установление ro-подключения к monitor-db
-        String monitorDbPath = "persistent_files\\at-monitor.db";
-        Connection monitorConnection = connectDB(monitorDbPath, true);
-        if (monitorConnection == null) {
-            log.error("Could not connect to monitor DB at {" + monitorDbPath + "}" );
-            log.fatal("At-worker app failed");
-        }
-        log.info("Monitor DB connected {" + monitorDbPath + "}");
-        // установление подключения к binance
 
-        // с момента подключения к worker-db логи писать туда
+        workerDbConnection = connectWorkerDB();
+        if (workerDbConnection == null) {
+            return;
+        }
+
+        // установление подключения к binance
+        webSocket = startSocket();
+        if (webSocket == null) {
+            log.error("Could not open websocket connection");
+            log.fatal("At-worker app failed");
+            return;
+        }
+        log.info("Websocket connection is open");
+
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            log.error(e);
+        }
+
+
+
+        webSocket.abort();
+        log.info("Websocket connection closed");
+
+        disconnectWorkerDB();
+        disconnectMonitorDB();
 
         log.info("At-worker finished");
 
@@ -49,10 +73,81 @@ public class ATWorkerApp {
             SQLiteConfig config = new SQLiteConfig();
             config.setReadOnly(readonly);
             connection = DriverManager.getConnection(url, config.toProperties());
+            connection.setAutoCommit(false);
         } catch (SQLException e) {
             log.error(e);
         }
         return connection;
+    }
+
+    private static Connection connectMonitorDB() {
+        // установление ro-подключения к monitor-db
+        String monitorDbPath = "persistent_files\\at-monitor.db";
+        Connection monitorConnection = connectDB(monitorDbPath, true);
+        if (monitorConnection == null) {
+            log.error("Could not connect to monitor DB at {" + monitorDbPath + "}" );
+            log.fatal("At-worker app failed");
+        } else {
+            log.info("Monitor DB connected {" + monitorDbPath + "}");
+        }
+        return monitorConnection;
+    }
+
+    private static Connection connectWorkerDB() {
+        String workerDbPath = "persistent_files\\at-worker.db";
+        Connection workerConnection = connectDB(workerDbPath, false);
+        if (workerConnection == null) {
+            log.error("Could not connect to worker DB at {" + workerDbPath + "}" );
+            log.fatal("At-worker app failed");
+        } else {
+            log.info("Worker DB connected {" + workerDbPath + "}");
+        }
+        return workerConnection;
+    }
+
+    private static void disconnectWorkerDB() {
+        // закрыть соединение с бд воркер
+        try {
+            workerDbConnection.close();
+        } catch (SQLException e) {
+            log.error(e);
+        }
+        log.info("Worker DB disconnected");
+    }
+
+    private static void disconnectMonitorDB() {
+        // закрыть соединение с бд монитор
+        try {
+            monitorDbConnection.close();
+        } catch (SQLException e) {
+            log.error(e);
+        }
+        log.info("Monitor DB disconnected");
+    }
+
+    public static WebSocket startSocket() {
+
+        WebSocket.Listener wsListener = new WebSocket.Listener() {
+            @Override
+            public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+                log.info(data);
+                webSocket.request(1);
+                return null;
+            }
+        };
+
+        String wsUri = "wss://stream.binance.com:9443/ws/btcusdt@bookTicker";
+
+        WebSocket webSocket = null;
+        try {
+            webSocket = HttpClient.newHttpClient()
+                    .newWebSocketBuilder().buildAsync(URI.create(wsUri), wsListener)
+                    .get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e);
+        }
+
+        return webSocket;
     }
 
 }

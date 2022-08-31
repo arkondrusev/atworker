db8d8693c045f22c8f52c74fb0ff081b2eb882d8
Artem <artem.kondrusev@gmail.com>
2022-09-01 00:16:22 +0300

    --

--- dev/src/test/java/com/sadkoala/arbitrator/atworker/RouteProfitCalculatorTest.java
+++ dev/src/test/java/com/sadkoala/arbitrator/atworker/RouteProfitCalculatorTest.java
@@ -4,13 +4,15 @@ import com.sadkoala.arbitrator.atworker.model.BigDecimal2;
 import com.sadkoala.arbitrator.atworker.model.PairPriceSlice;
 import com.sadkoala.arbitrator.atworker.model.RoutePriceSliceProfit;
 import com.sadkoala.arbitrator.atworker.model.Token;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RouteProfitCalculatorTest {
 
-    //@Test
+    @Test
     public void calcRouteProfitTest() {
         String token1 = "USDT";
         String token2 = "BTC";
@@ -29,10 +31,25 @@ public class RouteProfitCalculatorTest {
         RoutePriceSliceProfit routePriceSliceProfit = RouteProfitCalculator.calcRouteProfit(Global.stockExchange,
                 Global.findRouteByTokenNames(token1, token2, token3).get(),
                 priceSliceList);
-        System.out.println(routePriceSliceProfit.getProfitPct());
+        Assertions.assertNull(routePriceSliceProfit);
+
+        priceSliceList = new ArrayList<>();
+        pairPriceSlice = new PairPriceSlice(1659702901492l, "ethusdt", new BigDecimal2("0.9"), new BigDecimal2("0.7"));
+        pairPriceSlice.setPair(Global.findPairByTokenNames("ETH", "USDT").get());
+        priceSliceList.add(pairPriceSlice);
+        pairPriceSlice = new PairPriceSlice(1659702901492l, "ethbtc", new BigDecimal2("1.2"), new BigDecimal2("1"));
+        pairPriceSlice.setPair(Global.findPairByTokenNames("ETH", "BTC").get());
+        priceSliceList.add(pairPriceSlice);
+        pairPriceSlice = new PairPriceSlice(1659702901492l, "btcusdt", new BigDecimal2("1.2"), new BigDecimal2("1"));
+        pairPriceSlice.setPair(Global.findPairByTokenNames("BTC", "USDT").get());
+        priceSliceList.add(pairPriceSlice);
+        routePriceSliceProfit = RouteProfitCalculator.calcRouteProfit(Global.stockExchange,
+                Global.findRouteByTokenNames(token1, token2, token3).get(),
+                priceSliceList);
+        Assertions.assertEquals("0.10859", routePriceSliceProfit.getProfitPct().toString());
     }
 
-    //@Test
+    @Test
     public void doTradeTest() {
         Token usdt = Global.findTokenByName("usdt").get();
         Token eth = Global.findTokenByName("eth").get();
@@ -40,8 +57,12 @@ public class RouteProfitCalculatorTest {
         BigDecimal2 tradeFeeMultiplier = BigDecimal2.ONE.subtract(Global.stockExchange.getTradeFee());
         PairPriceSlice pairPriceSlice = new PairPriceSlice(1659702901492l, "ethusdt", new BigDecimal2("1.1"), new BigDecimal2("0.9"));
         pairPriceSlice.setPair(Global.findPairByTokens(usdt, eth).get());
+
         BigDecimal2 targetValue = RouteProfitCalculator.doTrade(usdt, eth, sourceValue, tradeFeeMultiplier, pairPriceSlice);
-        System.out.println(targetValue);
+        Assertions.assertEquals("0.908408", targetValue.toString());
+
+        targetValue = RouteProfitCalculator.doTrade(eth, usdt, sourceValue, tradeFeeMultiplier, pairPriceSlice);
+        Assertions.assertEquals("0.899325", targetValue.toString());
     }
 
 }

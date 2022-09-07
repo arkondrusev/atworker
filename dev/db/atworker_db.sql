d3481023cb7f1a699072aa9377c283974e175613
Artem <artem.kondrusev@gmail.com>
2022-09-08 00:18:59 +0300

    - calc profit check-create profit table

--- dev/db/atworker_db.sql
+++ dev/db/atworker_db.sql
@@ -2,14 +2,17 @@ create table worker_log (timestamp integer not null, tag text, msg text not null
 
 create index worklog_tmstmp_ix on worker_log (timestamp);
 
-create table at_prices (timestamp integer not null, pair text not null, best_ask real not null, best_bid real not null);
+create table at_price (timestamp integer not null, pair text not null, best_ask real not null, best_bid real not null);
 
-create index atprices_tmstmp_ix on at_prices (timestamp);
+create index atprice_tmstmp_ix on at_prices (timestamp);
 
 create table worker_state (key text unique not null, value text);
 
 create index workstate_key_ix on worker_state (key);
 
+/*
+create table at_profit (timestamp integer not null, route text not null, profit_pct integer not null);
+*/
 
 create table stock_exchange (
 	se_id integer primary key not null,
--- dev/src/main/java/com/sadkoala/arbitrator/atworker/CalcDBRoutesProfitApp.java
+++ dev/src/main/java/com/sadkoala/arbitrator/atworker/CalcDBRoutesProfitApp.java
@@ -1,14 +1,43 @@
 package com.sadkoala.arbitrator.atworker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CalcDBRoutesProfitApp {
 
+    private static final Logger log = LogManager.getLogger(CalcDBRoutesProfitApp.class);
+
+    private static final String TABLE_NAME_ROUTE_PROFITS = "at_profit";
+
+    private static final String SQL_CHECK_ROUTE_PROFITS_TABLE_EXISTS = "select name from sqlite_master where type='table' and name='" + TABLE_NAME_ROUTE_PROFITS + "'";
+
     public static void main(String[] args) throws SQLException {
 
-        GlobalResources.openWorkerDbConnection("atworker.db", false);
+        log.info("CalcDBRoutesProfitApp start");
 
-        
+        Connection conn = GlobalResources.openWorkerDbConnection("db/atworker.db", false);
+
+        // check route profits table exists
+        Statement tableCheckStmt = conn.createStatement();
+        ResultSet tableCheckRs = tableCheckStmt.executeQuery(SQL_CHECK_ROUTE_PROFITS_TABLE_EXISTS);
+        if (!tableCheckRs.next()) {
+            // create table for processing results
+            log.info("Table " + TABLE_NAME_ROUTE_PROFITS + " not found. Creating table.");
+            Statement tableCreateStmt = conn.createStatement();
+            tableCreateStmt.executeUpdate("create table " + TABLE_NAME_ROUTE_PROFITS + " (timestamp integer not null, route text not null, profit_pct integer not null);");
+            conn.commit();
+            tableCreateStmt.close();
+        }
+        tableCheckRs.close();
+
+        // read property "last processed timestamp
+        Statement readLastProcessed = conn.createStatement();
+
 
         /*
         * we need app property - last processed by calc routes timestamp
@@ -29,7 +58,8 @@ public class CalcDBRoutesProfitApp {
         *
         * */
 
-
+        GlobalResources.closeWorkerDbConnection();
+        log.info("CalcDBRoutesProfitApp finish");
     }
 
 }
